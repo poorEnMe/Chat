0f33fdc9cc41044a39b9354bed35644bbbcfe491
poorEnMe <617679198@QQ.COM>
2017-09-30 17:31:49 +0800

    fullStep1

--- ChatServer.js
+++ ChatServer.js
@@ -1,9 +1,18 @@
 const app = require('express')();
-
 const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
 
 app.get('/',function (req,res) {
-    res.send('<h1>Hello</h1>');
+    res.sendFile(__dirname + '/view/index.html');
+});
+
+//io.on('connection',(Socket)=>console.log('a user connected'));
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+      console.log(socket.username);
+      io.emit('chat message',msg);
+  });
 });
 
 http.listen(3000,()=>console.log('listen:3000'));
